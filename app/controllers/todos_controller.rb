2f51a77c8c592b4f3c22b1463fc544b9f2308357
matthewmjm <matthewmjm@yahoo.com>
2020-12-01 13:36:41 -0700

    create and destroy actions

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,5 +4,21 @@ class TodosController < ApplicationController
         @todos = Todo.all
         render json: @todos
     end
-    
+
+    def create
+        @todo = Todo.create(
+            title: params[:title],
+            content: params[:content],
+            urgent: params[:urgent],
+            done: params[:done]
+        )
+        render json: @todo, status: :created
+    end
+
+    def destroy
+        @todo = Todo.find(params[:id])
+        @todo.destroy
+        render status: :no_content
+    end
+
 end
