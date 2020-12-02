88b4874df50fcf58cc5c31fc95f5961e223d2600
matthewmjm <matthewmjm@yahoo.com>
2020-12-02 11:43:15 -0700

    sign-up functionality

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+        render json: @users
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.valid?
+            @user.save
+            @token = JWT.encode({user_id: @user.id}, "yerrr")
+            render json: {user: @user, token: @token}, status: :created
+        else
+            render json: {errors: @user.errors.full_messages}, status: :not_acceptable
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
+end
