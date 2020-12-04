db154e0cca78a49d6a1ab2034ad04eef1ad19667
matthewmjm <matthewmjm@yahoo.com>
2020-12-03 21:26:23 -0700

    define login action for backend authorization of username and password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+
+    def login
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            @token = JWT.encode({user_id: @user.id}, "yerrr")
+            render json:  {user: @user, token: @token}, status: :accepted
+        else
+            render json: {message: "Invalid username or password"}, status: :unauthorized
+        end
+    end
+
 end
